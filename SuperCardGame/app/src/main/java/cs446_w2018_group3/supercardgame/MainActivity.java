9c95720dad65ae0419d4dcfaa8cd17b9f6acb7ad
flycounter <flycounter@hotmail.com>
2018-01-21 22:23:23 -0500

    Project Init
    Project Initialization

---
+++ SuperCardGame/app/src/main/java/cs446_w2018_group3/supercardgame/MainActivity.java
@@ -0,0 +1,13 @@
+package cs446_w2018_group3.supercardgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
