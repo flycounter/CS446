5a17c5f2e4a095b2ba396be03049f108be37f9aa
liansha <j28xie@uwaterloo.ca>
2018-02-23 18:04:13 -0500

    v1.0

--- SuperCardGame/app/src/main/java/cs446_w2018_group3/supercardgame/MainActivity.java
+++ SuperCardGame/app/src/main/java/cs446_w2018_group3/supercardgame/MainActivity.java
@@ -1,13 +1,22 @@
 package cs446_w2018_group3.supercardgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //test code
+        Intent intent = new Intent(MainActivity.this,SinglePlayActivity.class);
+        startActivity(intent);
+        finish();
     }
+    }
+
 }
