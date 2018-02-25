1e931cc5dbdf9bfd2c7cebab7da2c056ae30b6af
Yandong Zhu <pigheadzyd@gmail.com>
2018-02-24 19:22:55 -0500

    Adding Card Id reference

--- SuperCardGame/app/src/main/java/cs446_w2018_group3/supercardgame/model/Card.java
+++ SuperCardGame/app/src/main/java/cs446_w2018_group3/supercardgame/model/Card.java
@@ -3,6 +3,21 @@ package cs446_w2018_group3.supercardgame.model;
 /**
  * Created by JarvieK on 2018/2/23.
  */
+//  Card Id Reference:
+//  0: Water
+//  1: Fire
+//  2: Air
+//  3: Dirt
+//  4: Aqua
+//  5: Steam
+//  6: Ice
+//  7: Mud
+//  8: Flame
+//  9: Blast
+//  10: Lava
+//  11: Gale
+//  12: Sand
+//  13: Rock
 
 public class Card {
     abstract class BaseCard implements IModel {
